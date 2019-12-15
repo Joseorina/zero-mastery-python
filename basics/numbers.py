ce2c574b8096efa40d4186965b0207d7a7924500
Joseorina <orinangare@gmail.com>
2019-12-15 19:11:10 +0300

    [python basics reminder]

--- basics/numbers.py
+++ basics/numbers.py
@@ -6,11 +6,13 @@
 
 # float
 # print(type(2/4))
-a = 20 + 0.1
-print(type(a))
-b = 9.9+0.1
-print(b)
-print(type(b))
-print( 2 ** 3)
-print(2 // 5)
-print(5 % 3)
+# a = 20 + 0.1
+# print(type(a))
+# b = 9.9+0.1
+# print(b)
+# print(type(b))
+# print( 2 ** 3)
+# print(2 // 5)
+# print(5 % 3)
+print(round(2.5))
+print(abs(-30))
