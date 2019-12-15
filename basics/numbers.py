e883ad288b88ee223d2cea13b0908403a33cd692
Joseorina <orinangare@gmail.com>
2019-12-15 19:08:19 +0300

    [python basics reminder]

--- basics/numbers.py
+++ basics/numbers.py
@@ -1,8 +1,16 @@
 # Fundamental data types
 # integers
-print(type(2+4))
-print(type(2-4))
-print(type(2*4))
-print(type(2/4))
+# print(type(2+4))
+# print(type(2-4))
+# print(type(2*4))
 
 # float
+# print(type(2/4))
+a = 20 + 0.1
+print(type(a))
+b = 9.9+0.1
+print(b)
+print(type(b))
+print( 2 ** 3)
+print(2 // 5)
+print(5 % 3)
